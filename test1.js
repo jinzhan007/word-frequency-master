d3196b91de6ba6496577817ddf2a237d64326c5f
jinzhan <420466986@qq.com>
2017-05-18 19:02:10 +0800

    test1.js提交

---
+++ test1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Admin on 2017/4/17.
+ */
+/*
+"use strict"
+var _=require("lodash")
+var chai=require("chai")
+var sinon=require("sinon")
+var sinonChai=require("sinon-chai")
+var expect=chai.expect
+chai.use(sinonChai)
+
+var main=require("./test.js")
+*/
+describe("fn", function () {
+    it("testcase1", function () {
+        expect("").toEqual(fn(""));
+    });
+});
