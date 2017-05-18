1e94b55a58dfe38d9f5f2ee875a9cc93218e1eeb
jinzhan <420466986@qq.com>
2017-05-18 19:18:21 +0800

    test6.js提交

---
+++ test6.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Admin on 2017/4/17.
+ */
+/*
+"use strict"
+var _=require("lodash")
+var chai=require("chai")
+var sinon=require("sinon")
+var sinonChai=require("sinon-chai")
+var expect=chai.expect
+chai.use(sinonChai)
+
+var main=require("./test.js")
+*/
+describe("fn", function () {
+    it("testcase1", function () {
+        expect("").toEqual(fn(""));
+    });
+    it("testcase2", function () {
+        expect("输入的类型不是字符串").toEqual(fn(123));
+    });
+    it("testcase3", function () {
+        expect("输入的类型不是字符串").toEqual(fn(12.3));
+    });
+    it("testcase4", function () {
+        var testarry=new Array([1,"2"])
+        expect("输入的类型不是字符串").toEqual(fn(testarry));
+    });
+    it("testcase5", function () {
+        expect("请不要输入缩写").toEqual(fn("it's a Pig"));
+    });
+    it("testcase6", function () {
+        expect("it 1\nis 1\na 1\npig 1\n").toEqual(fn("IT     IS A PIG"));
+    });
+});
