fa893bde829bda0d789195fa70fae3c84266c9df
jinzhan <420466986@qq.com>
2017-05-18 19:21:31 +0800

    test9.js提交

---
+++ test9.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Admin on 2017/4/17.
+ */
+/*
+"use strict"
+var _=require("lodash")
+var chai=require("chai")
+var sinon=require("sinon")
+var sinonChai=require("sinon-chai")
+var expect=chai.expect
+chai.use(sinonChai)
+
+var main=require("./test.js")
+*/
+describe("fn", function () {
+    it("testcase1", function () {
+        expect("").toEqual(fn(""));
+    });
+    it("testcase2", function () {
+        expect("输入的类型不是字符串").toEqual(fn(123));
+    });
+    it("testcase3", function () {
+        expect("输入的类型不是字符串").toEqual(fn(12.3));
+    });
+    it("testcase4", function () {
+        var testarry=new Array([1,"2"])
+        expect("输入的类型不是字符串").toEqual(fn(testarry));
+    });
+    it("testcase5", function () {
+        expect("请不要输入缩写").toEqual(fn("it's a Pig"));
+    });
+    it("testcase6", function () {
+        expect("it 1\nis 1\na 1\npig 1\n").toEqual(fn("IT     IS A PIG"));
+    });
+    it("testcase7", function () {
+        expect("输入的字符串包含非法字符").toEqual(fn("IT% IS A PIG"));
+    });
+    it("testcase8", function () {
+        expect("输入的字符串包含非法字符").toEqual(fn("it is a pig,it is cute"));
+    });
+    it("testcase9", function () {
+        expect("it 3\nwas 2\nthe 2\nage 2\nof 2\nwisdom 1\nfoolishness 1\nis 1\n").toEqual(fn("it was the age of wisdom it was the age of foolishness it is"));
+    });
+});
