4feaa7218ec41323550ad1b01cfb14a194569460
jinzhan <420466986@qq.com>
2017-05-18 19:10:32 +0800

    test3.js提交

---
+++ test3.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Admin on 2017/4/17.
+ */
+/*
+"use strict"
+var _=require("lodash")
+var chai=require("chai")
+var sinon=require("sinon")
+var sinonChai=require("sinon-chai")
+var expect=chai.expect
+chai.use(sinonChai)
+
+var main=require("./test.js")
+*/
+describe("fn", function () {
+    it("testcase1", function () {
+        expect("").toEqual(fn(""));
+    });
+    it("testcase2", function () {
+        expect("输入的类型不是字符串").toEqual(fn(123));
+    });
+    it("testcase3", function () {
+        expect("输入的类型不是字符串").toEqual(fn(12.3));
+    });
+});
